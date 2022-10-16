43a467b25567ab39402764901aa61e2794ba9a25
Marksolidor <marco350sl@gmail.com>
2022-10-16 08:21:22 -0300

    feat pizza page views by id

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,46 +1,54 @@
-import React from 'react'
+import React from "react";
 import { Col, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { ProductsContext } from '../context/InfoProvider'
-
+import { ProductsContext } from "../context/InfoProvider";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  const { products } = React.useContext(ProductsContext)
+  const { products } = React.useContext(ProductsContext);
+  const navigate = useNavigate();
 
 
   return (
-<>
+    <>
       <h1>Home</h1>
       <Row md={2} xs={1} lg={3} className="g-3">
         {products.map((item) => (
-           <Col key={item.id}>
-           <div className="card-container">
-           <div>
-                           <img className="img-fluid" src={item.img}></img>
-                       </div>
-                       <div>
-                         <h2>{item.name} </h2>
-                         <hr></hr>
-                         <h4>Ingredientes</h4>
-                         <ul>
-                           {item.ingredients.map((ingredient) => (
-                             <li key={ingredient}>🍕 {ingredient}</li>
-                           ))}
-                         </ul>
-                       </div>
-                       <div>
-                           <span>{item.price}</span>
-                       </div>
-                       <div className="buttons">
-                       <button type="button" class="btn btn-primary">Ver más</button>
-                       <button type="button" class="btn btn-success">🛒 Agregar</button>
-                       </div>
-           </div>
-                     </Col>
-                   ))}
-                 </Row>
-               </>
-             );
-           }
-           
-           export default Home;
+          <Col key={item.id}>
+            <div className="card-container">
+              <div>
+                <img className="img-fluid" alt={item.name} src={item.img}></img>
+              </div>
+              <div>
+                <h2>{item.name} </h2>
+                <hr></hr>
+                <h4>Ingredientes</h4>
+                <ul>
+                  {item.ingredients.map((ingredient) => (
+                    <li key={ingredient}>🍕 {ingredient}</li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <span>{item.price}</span>
+              </div>
+              <div className="buttons">
+                <button type="button" class="btn btn-primary"
+                //onChange={(event) => goPizza(event.target.value)}
+                onClick={() => navigate(`/pizza/${item.id}`)}
+                >
+                  Ver más
+                </button>
+                <button type="button" class="btn btn-success">
+                  🛒 Agregar
+                </button>
+              </div>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default Home;
--- src/views/Pizza.jsx
+++ src/views/Pizza.jsx
@@ -1,3 +1,41 @@
-export default function Pizza () {
-    return <h1>Pizza</h1>
-}
+import React, { useContext } from "react";
+import { ProductsContext } from "../context/InfoProvider";
+import { useParams } from "react-router-dom";
+
+const Pizza = () => {
+    const { products } = useContext(ProductsContext);
+    const { id } = useParams();
+  
+    return (
+      <div className="product-container">
+          {products.filter((a) => a.id === id)
+            .map((item) => (
+              <div className="img-container">
+                <img className="img-fluid" src={item.img} alt={item.name} />
+                <div className="info-container">
+                  <div className="name-container">
+                  <h2>{item.name} </h2>
+                    <hr></hr>
+                  </div>
+                  <div className="desc-container">
+                    <p>{item.desc}</p>
+                  </div>
+                  <div className="topping-container">
+                    <h3>Ingredientes</h3>
+                    <ul>
+                  {item.ingredients.map((ingredient) => (
+                    <li key={ingredient}>🍕 {ingredient}</li>
+                  ))}
+                </ul>
+                <div className="price-container">
+                <h2>${item.price}</h2>
+                </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+      </div>
+    );
+  };
+
+  export default Pizza;
